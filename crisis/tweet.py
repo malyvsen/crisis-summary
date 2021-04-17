5d9ce232ec1fe31a75b2bba57ded4384150b661a
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-04-17 17:02:23 +0200

    fix: tweet description parsing

--- crisis/tweet.py
+++ crisis/tweet.py
@@ -12,9 +12,14 @@ class Tweet:
 
     @classmethod
     def from_rss(cls, rss_item: dict):
+        description_soup = BeautifulSoup(rss_item["description"], "html.parser")
         return cls(
             title=rss_item["title"],
-            description=BeautifulSoup(rss_item["description"], "html.parser").p.text,
+            description=(
+                description_soup.p.text
+                if description_soup.p is not None
+                else description_soup.text
+            ),
             user=rss_item["dc:creator"],
             time=datetime.strptime(rss_item["pubDate"], "%a, %d %b %Y %H:%M:%S %Z"),
         )
--- tests/test_feed.py
+++ tests/test_feed.py
@@ -2,5 +2,5 @@ from crisis import Feed
 
 
 def test_soufriere():
-    feed = Feed.from_rss_url("https://rss.app/feeds/GKo6EkBtnIYLw1hY.xml")
+    feed = Feed.from_rss_url("https://rss.app/feeds/wTXzZFTDgpaDTNTc.xml")
     assert len(feed.tweets[0].description) > 0
