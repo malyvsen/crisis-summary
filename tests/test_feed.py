5c77e74e285755073b7395337cfb8d670d7e07a0
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-04-17 17:19:10 +0200

    test: feed changes

--- tests/test_feed.py
+++ tests/test_feed.py
@@ -1,6 +1,20 @@
-from crisis import Feed
+from datetime import datetime
+from crisis import Feed, Tweet
 
 
-def test_soufriere():
+def test_from_url():
     feed = Feed.from_rss_url("https://rss.app/feeds/wTXzZFTDgpaDTNTc.xml")
-    assert len(feed.tweets[0].description) > 0
+    assert len(feed.tweets) > 0
+
+
+def test_union():
+    hi = Tweet(
+        title="Hi!", description="", user="programmer", time=datetime(2020, 12, 3)
+    )
+    hello = Tweet(
+        title="Hello!", description="", user="programmer", time=datetime(2020, 12, 2)
+    )
+    cow = Tweet(title="Hi!", description="", user="cow", time=datetime(2020, 12, 1))
+    assert Feed.union(Feed(tweets=[hi, hello]), Feed(tweets=[hi, cow])) == Feed(
+        tweets=[hi, hello, cow]
+    )
